6dbb13158794bdff7e8644f629d1dfe36e9c340b
Flavio Pires <flavioasp77@gmail.com>
2021-06-23 19:00:34 -0300

    Exercicio 1 finalizado

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/exercicio-1.js
@@ -0,0 +1,8 @@
+let a = 5;
+let b = 10;
+
+console.log("a adição é " + (a + b));
+console.log("a subtração é " + (a - b));
+console.log("a multiplicação é " + (a * b));
+console.log("a divisão é " + (a / b));
+console.log("o módulo é " + (a % b));
--- exercises/introducao-a-javascript-e-logica-de-programacao_1/scripts.js
+++
@@ -1,4 +0,0 @@
-let num1 = 5;
-let num2 = 10;
-
-console.log(num1);
