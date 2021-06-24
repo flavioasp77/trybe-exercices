f7d88b77f77b4fe85b69e4ae6278110a4d4a9e76
Flavio Pires <flavioasp77@gmail.com>
2021-06-24 16:00:08 -0300

    exercicio 9 finalizado

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/exercico-9.js
@@ -0,0 +1,9 @@
+let num1 = 1;
+let num2 = 6;
+let num3 = 4;
+
+if (num1 % 2 === 1 || num2 % 1 === 1 || num3 % 2 === 1){
+  console.log("Verdadeiro - pelo meno um numero é ímpar!")
+} else{
+  console.log("Falso - todos os numeros são par!");
+}
