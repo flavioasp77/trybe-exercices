204db1700c63bbe4d48c6607989d134f14234309
Flavio Pires <flavioasp77@gmail.com>
2021-06-24 15:22:22 -0300

    exercicio 7 finalizado

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/exercicio-7.js
@@ -0,0 +1,17 @@
+let nota = 91;
+
+if (nota >= 90 && nota <= 100){
+  console.log("Conceito A.");
+}else if (nota >= 80 && nota < 90){
+  console.log("Conceito B.");
+}else if (nota >= 70 && nota < 80){
+  console.log("Conceito C.");
+}else if (nota >= 60 && nota < 70){
+  console.log("Conceito D.");
+}else if (nota >= 50 && nota < 60){
+  console.log("Conceito E.");
+}else if (nota >-1 && nota <50){
+  console.log("Conceito F.");
+}else{
+  console.log("Erro, o programa será encerrado");
+}
