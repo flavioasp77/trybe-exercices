1a0f1a693dea8b06a76b1b6821818d6e7ad3f04b
Flavio Pires <flavioasp77@gmail.com>
2021-06-24 15:32:11 -0300

    Exercicio 8 finalizado

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/exercicio8.js
@@ -0,0 +1,9 @@
+let num1 = 3;
+let num2 = 1;
+let num3 = 7;
+
+if (num1 % 2 === 0 || num2 % 2 === 0 || num3 % 2 === 0){
+  console.log("Verdadeiro - peno meno um numero é par!")
+} else{
+  console.log("Falso - todos os numeros são ímpares!");
+}
