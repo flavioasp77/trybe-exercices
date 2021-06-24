5320a4bbcb39de491c19e17d459bd600004b4e07
Flavio Pires <flavioasp77@gmail.com>
2021-06-24 14:31:41 -0300

    Exercicio 6 finalizado

--- exercises/introducao-a-javascript-e-logica-de-programacao_1/exercicio-6.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/exercicio-6.js
@@ -1,25 +1,25 @@
-let peça = "peão";
+let peça = "Bispo";
 
-switch (peça) {
-  case "peão":
+switch (peça.toUpperCase()) {
+  case "PEÃO":
     console.log("O peão se movimenta somente para frente, uma ou duas casas no seu lance inicial e depois apenas uma.");
     break;
-  case "cavalo":
+  case "CAVALO":
     console.log("o cavalo se movimenta formando um 'L', sendo dua casas em uma direção e uma casa para o lado.");
     break;
-  case "bispo":
+  case "BISPO":
     console.log("o bispo se movimenta na diagonal quantas casas quiser e também pode voltar.");
     break;
-  case "torre":
+  case "TORRE":
     console.log("A torre se movimenta na hrizontal ou na vertical quantas casas quiser e também pode voltar.");
     break;
-  case "rainha":
+  case "RAINHA":
     console.log("A rainha se movimenta igual ao bispo e a torre.")
     break;
-  case "dama":
+  case "DAMA":
     console.log("A dama se movimenta igual ao bispo e a torre.")
     break;
-  case "rei":
+  case "REI":
     console.log("O rei movimenta igual a rainha ou dama, porém uma casa de cada vez.")
     break;
   default:
