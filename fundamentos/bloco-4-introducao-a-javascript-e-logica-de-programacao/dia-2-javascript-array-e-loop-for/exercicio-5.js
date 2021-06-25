01c22e0c35373ae67f3317ca2a09d61b081e2b7e
Flavio Pires <flavioasp77@gmail.com>
2021-06-25 16:14:33 -0300

    exercicio 5 finalizado

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicio-5.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicio-5.js
@@ -1,9 +1,9 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let resultado = 0;
+let maior = 0;
 
-//soma os valores do array numbers e coloca na variavel resultado
 for (let index = 0; index < numbers.length; index += 1) {
-  resultado += numbers[index];
+  if (numbers[index] > maior) {
+    maior = numbers[index];
+  }
 }
-//apresenta a media aritmética dos valores do array numbers
-console.log(resultado / numbers.length);
+console.log(maior);
