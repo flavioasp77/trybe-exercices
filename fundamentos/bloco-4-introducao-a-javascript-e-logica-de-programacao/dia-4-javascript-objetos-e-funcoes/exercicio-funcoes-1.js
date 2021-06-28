04ad341399bab958de58d52deb3f8db6601ac8c3
Flavio Pires <flavioasp77@gmail.com>
2021-06-28 20:04:55 -0300

    exercicio funços 1 finalizado

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercicio-funcoes-1.js
@@ -0,0 +1,12 @@
+function palindromo(palavra) {
+  let aux = "";
+  for (let index = palavra.length - 1; index >= 0; index -= 1) {
+    aux += palavra[index];
+    }
+    if( palavra === aux){
+      return console.log(true);
+    }else{
+      return console.log(false);
+    }
+  }
+palindromo("ovo");
