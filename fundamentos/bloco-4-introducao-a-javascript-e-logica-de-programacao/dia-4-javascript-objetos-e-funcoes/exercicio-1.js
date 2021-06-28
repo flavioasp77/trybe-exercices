f240cd0bac29ec7c6702fe577200d5a6b433f17b
Flavio Pires <flavioasp77@gmail.com>
2021-06-28 19:23:32 -0300

    exercicio 5 finalizado

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercicio-1.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercicio-1.js
@@ -1,12 +1,12 @@
-let info = {
-  personagem: 'Margarida',
-  origem: 'Pato Donald',
-  nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
-};
+// let info = {
+//   personagem: 'Margarida',
+//   origem: 'Pato Donald',
+//   nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+// };
 
 //console.log("Bem vinda,", info.personagem);
 
-info.recorrente = 'sim';
+//info.recorrente = 'sim';
 
 //console.log(info);
 
@@ -14,6 +14,28 @@ info.recorrente = 'sim';
 //   console.log(key);
 // }
 
-for( key in info){
-  console.log(info[key]);
+// for( key in info){
+//   console.log(info[key]);
+// }
+
+let info = {
+  personagem: 'Margarida',
+  origem: 'Pato Donald',
+  nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+  recorrente: 'sim'
+};
+
+let info2 = {
+  personagem: 'Tio Patinhas',
+  origem: "Pato DonalChristmas on Bear Mountain, Dell's Four Color Comics #178d",
+  nota: 'O último MacPatinhas',
+  recorrente: 'sim'
+};
+
+for (let key in info, info2){
+  if( info[key] === info2[key]){
+    console.log("ambos recorrentes");
+  }else {
+    console.log(info[key] + ' e ' + info2[key]);
+  }  
 }
