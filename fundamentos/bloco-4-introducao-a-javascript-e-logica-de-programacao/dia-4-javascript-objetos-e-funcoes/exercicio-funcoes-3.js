43f7d96180cfcbb13f236c626e28811fa8deca0b
Flavio Pires <flavioasp77@gmail.com>
2021-07-05 15:51:25 -0300

    alualizando

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercicio-funcoes-3.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercicio-funcoes-3.js
@@ -1,16 +1,16 @@
 function indiceDoMaiorNumero(array) {
-  let maiorNumero = 0;
+  let menorNumero;
   let indice;
   for (let index = 0; index < array.length; index += 1) {
-    
-    if (array[index] > maiorNumero) {
-      maiorNumero = array[index];
+    menorNumero = array[index];
+    if (array[index] < menorNumero) {
+      menorNumero = array[index];
     }
-    if (array[index] == maiorNumero) {
+    if (array[index] == menorNumero) {
       indice = index;
     }
   }
   return console.log(indice);
 }
-let numeros = [2, 3, 6, 7, 10, 1];
+let numeros = [2, 4, 6, 7, 10, 8, 5, 20];
 indiceDoMaiorNumero(numeros);
