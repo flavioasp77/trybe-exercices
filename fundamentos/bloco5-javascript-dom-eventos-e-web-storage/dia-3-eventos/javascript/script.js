7beb46438a120dacbab31a076a3d490392bdd871
Flavio Pires <flavioasp77@gmail.com>
2021-07-07 15:57:03 -0300

    Exercicio 3 finalizado

--- fundamentos/bloco5-javascript-dom-eventos-e-web-storage/dia-3-eventos/javascript/script.js
+++ fundamentos/bloco5-javascript-dom-eventos-e-web-storage/dia-3-eventos/javascript/script.js
@@ -14,7 +14,7 @@ function createDaysOfTheWeek() {
 createDaysOfTheWeek();
 
 // Escreva seu código abaixo.
-
+// Exercicio 01
 function createDaysOfMouth() {
   const days = document.querySelector('#days');
   const daysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
@@ -26,7 +26,7 @@ function createDaysOfMouth() {
     if (day === 24 | day === 25 | day === 31) {
       createDays.classList.add('holiday');
     }
-    if (day === 4 | day === 11 | day === 18 | day === 25){
+    if (day === 4 | day === 11 | day === 18 | day === 25) {
       createDays.classList.add('friday');
     }
   }
@@ -34,13 +34,37 @@ function createDaysOfMouth() {
 
 createDaysOfMouth();
 
-function createButton(string){
+// Exercicio 02
+function createButton(string) {
   const getDiv = document.querySelector('.buttons-container');
-  const newButton  = document.createElement('button');
-  
+  const newButton = document.createElement('button');
+
   newButton.innerHTML = string;
   newButton.id = 'btn-holiday';
-  getDiv.appendChild(newButton); 
+  getDiv.appendChild(newButton);
 }
 
 createButton('Feriados');
+
+// Exercício 03
+
+function showHoliday() {
+  let getButton = document.querySelector('#btn-holiday'); // armazena o elemento que vai receber o evento (click)
+  let dayHoliday = document.querySelectorAll('.holiday'); // pega todos os elementos com a classe específica que terá seus atributos alterados
+  let setColor = "greenyellow"; // nova cor
+  let defautColor = "rgb(238,238,238)"; // cor original
+
+  getButton.addEventListener('click', function () {
+    for (let holiday of dayHoliday) { // cria um loop com todos os elementos na variavel dayHoliday
+      // faz as verificaçẽos para trocar as cores de fundo
+      if (holiday.style.backgroundColor === setColor) { 
+        holiday.style.backgroundColor = defautColor;
+      } else {
+        holiday.style.backgroundColor = setColor;
+      }
+    }
+  });
+}
+
+showHoliday();
+
