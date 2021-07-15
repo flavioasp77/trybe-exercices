43786f0e0a46de53d9d598d8927e26aa9d7b03c8
Flavio Pires <flavioasp77@gmail.com>
2021-07-15 19:14:41 -0300

    Criando as funções de validação

--- fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
+++ fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
@@ -1,24 +1,76 @@
 const statesBr = ["Acre", "Alagoas", "Amapá", "Amazonas", "Bahia", "Ceará", "Distrito Federal", "Espírito Santo", "Goiás", "Maranhão", "Mato Grosso", "Mato Grosso do Sul", "Minas Gerais", "Pará", "Paraíba", "Paraná", "Pernambuco", "Piauí", "Rio de Janeiro", "Rio Grande do Norte", "Rio Grande do Sul", "Rondônia", "Roraima", "Santa Catarina", "São Paulo", "Sergipe", "Tocantins"];
 
+let pass;
+
+function validName() {
+  let name = document.querySelector('#input-name').value;
+  (name = '') ? alert('Preencha o campo nome!!!'): pass = true;
+  return pass;
+}
+
+function validEmail() {
+  let email = document.querySelector('#input-email').value;
+  (email = '') ? alert('Preencha o campo e-mail!!!'): pass = true;
+  return pass;
+}
+
+function validAdress() {
+  let adress = document.querySelector('#input-adress').value;
+  (adress = '') ? alert('Preencha o campo endereço!!!'): pass = true;
+  return pass;
+}
+
+function validCity() {
+  let city = document.querySelector('#input-city').value;
+  (city = '') ? alert('Preencha o campo cidade!!!'): pass = true;
+  return pass;
+}
+
+function validState() {
+
+}
+
+// let state = document.querySelector('#state');
+// let type = document.querySelector('input[name="type"]:checked').value;
+// let sumary = document.querySelector('#input-sumary').value;
+// let office = document.querySelector('#input-office');
+
+
 function setStates() {
-  let listStatess = document.querySelector('#state');
+  let listStates = document.querySelector('#state');
   for (let index = 0; index < statesBr.length; index += 1) {
     let states = document.createElement('option');
     states.innerHTML = statesBr[index];
     states.setAttribute("value", statesBr[index]);
-    listStatess.appendChild(states);
+    listStates.appendChild(states);
   }
 }
 setStates()
 
 function validDate() {
-  let pass;
+
   let day = document.querySelector('#day').value;
   let mouth = document.querySelector('#mouth').value;
   let year = document.querySelector('year').value;
-  (day > 1 && day <= 31) ? pass = true : pass = false;
-  (mouth > 1 && mouth <= 12) ? pass = true : pass = false;
-  (year > 1900) ? pass = true : pass = false;
+
+  (day > 1 && day <= 31) ? pass = true: pass = false;
+  (mouth > 1 && mouth <= 12) ? pass = true: pass = false;
+  (year > 1900) ? pass = true: pass = false;
 
   return pass;
+}
+
+function createSection() {
+  const main = document.querySelector('main');
+  let section = document.createElement('section');
+  section.id = 'curricullun';
+  main.appendChild(section);
+}
+
+function sendForm(event) {
+  event.preventDefault();
+  if (validDate()) {
+    createSection();
+  } else {}
+
 }
