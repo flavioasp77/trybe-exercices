fb68269bf90f55eaf1112e6f68f01e696763002e
Flavio Pires <flavioasp77@gmail.com>
2021-08-12 20:06:34 -0300

    entendendo a função fetch

---
+++ fundamentos/bloco-9-javascript-assincrono-e-promises/dia-2-javascript-promises/apiScript..js
@@ -0,0 +1,16 @@
+// apiScript.js
+const API_URL = 'https://icanhazdadjoke.com/';
+
+const fetchJoke = () => {
+  // Adicionar lógica aqui!
+  const myObject = {
+    method: 'GET',
+    headers: { 'Accept': 'application/json' }
+  };
+
+  fetch(API_URL, myObject)
+    .then(response => console.log(response));
+
+};
+
+window.onload = () => fetchJoke();
