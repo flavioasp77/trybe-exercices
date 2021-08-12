762c111834679606db734c1ec5248beda85adb89
Flavio Pires <flavioasp77@gmail.com>
2021-08-12 19:26:51 -0300

    Atualizando exercícios

--- fundamentos/bloco-9-javascript-assincrono-e-promises/dia-1-javascript-assincrono-e-callbacks/script.js
+++ fundamentos/bloco-9-javascript-assincrono-e-promises/dia-1-javascript-assincrono-e-callbacks/script.js
@@ -79,6 +79,23 @@
 
 // ---------------------------------------------
 
+// const messageDelay = () => Math.floor(Math.random() * 5000);
+
+// const getMarsTemperature = () => {
+//   const maxTemperature = 58;
+//   return Math.floor(Math.random() * maxTemperature);
+// };
+
+// // crie a função sendMarsTemperature abaixo
+// const sendMarsTemperature = () => {
+//   const currTemprature = getMarsTemperature();
+//   setTimeout(() => console.log(`Mars temperature is: ${currTemprature} degree Celsius`),messageDelay())
+// }
+
+// sendMarsTemperature(); // imprime "Mars temperature is: 20 degree Celsius", por exemplo
+
+// -----------------------------------
+
 const messageDelay = () => Math.floor(Math.random() * 5000);
 
 const getMarsTemperature = () => {
@@ -86,10 +103,15 @@ const getMarsTemperature = () => {
   return Math.floor(Math.random() * maxTemperature);
 };
 
-// crie a função sendMarsTemperature abaixo
-const sendMarsTemperature = () => {
-  const currTemprature = getMarsTemperature();
-  setTimeout(() => console.log(`Mars temperature is: ${currTemprature} degree Celsius`),messageDelay())
-}
+const toFahrenheit = (degreeCelsius) => (degreeCelsius * 9/5) + 32;
 
-sendMarsTemperature(); // imprime "Mars temperature is: 20 degree Celsius", por exemplo
+const temperatureInFahrenheit = (temperature) =>
+  console.log(`It is currently ${toFahrenheit(temperature)}ºF at Mars`);
+
+const greet = (temperature) =>
+  console.log(`Hi there! Curiosity here. Right now is ${temperature}ºC at Mars`);
+
+// definição da função sendMarsTemperature...
+
+sendMarsTemperature(temperatureInFahrenheit); // imprime "It is currently 47ºF at Mars", por exemplo
+sendMarsTemperature(greet); // imprime "Hi there! Curiosity here. Right now is 53ºC at Mars", por exemplo
