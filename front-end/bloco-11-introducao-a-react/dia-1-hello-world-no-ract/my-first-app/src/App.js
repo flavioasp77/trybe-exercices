aa5322eaa40ee6fc6f3b16032071727c476b2773
Flavio Pires <flavioasp77@gmail.com>
2021-08-30 20:30:42 -0300

    Segundo exercicio de React

--- front-end/bloco-11-introducao-a-react/dia-1-hello-world-no-ract/my-first-app/src/App.js
+++ front-end/bloco-11-introducao-a-react/dia-1-hello-world-no-ract/my-first-app/src/App.js
@@ -1,16 +1,21 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 const Task = (value) => {
   return (
-    <li>{value}</li>
+    <li key={value}>{value}</li>
   );
 }
 
-function App() {
-  return (
-    Task('Qualquer')
-  );
+const tarefas = ['Acordar', 'Tomar café', 'Escovar os dentes', 'Ir trabalhar'];
+
+class App extends React.Component {
+  render() {
+    return (
+      <ul>{ tarefas.map(tarefa => Task(tarefa)) }</ul>
+    );
+  }
 }
 
 export default App;
