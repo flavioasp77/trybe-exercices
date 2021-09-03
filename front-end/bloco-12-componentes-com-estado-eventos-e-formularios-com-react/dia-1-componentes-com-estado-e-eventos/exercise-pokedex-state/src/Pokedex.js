6b8a3ac002eae531b4626c9728806ee89ec7d1b0
Flavio Pires <flavioasp77@gmail.com>
2021-09-03 14:21:56 -0300

    Atualizando repositório

--- front-end/bloco-12-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/exercise-pokedex-state/src/Pokedex.js
+++ front-end/bloco-12-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/exercise-pokedex-state/src/Pokedex.js
@@ -1,14 +1,38 @@
 import React from 'react';
+import pokemons from './data';
 import Pokemon from './Pokemon';
 
 class Pokedex extends React.Component {
-    render() {
-        return (
-            <div className="pokedex">
-                {this.props.pokemons.map(pokemon => <Pokemon key={pokemon.id} pokemon={pokemon} />)}
-            </div>
-        );
+  constructor() {
+    super();
+    this.state = {
+      count: 0,
     }
+    this.handleNextPokemon = this.handleNextPokemon.bind(this);
+  }
+  
+  handleNextPokemon() {
+      if (pokemons.length -1 > this.state.count) {
+      this.setState((preventState) => ({
+        count: preventState.count + 1,
+      }))
+    } else {
+      this.setState({
+        count: 0,
+      })
+    }
+  }
+  render() {
+    return (
+      <div>
+        <div className="pokedex">          
+         <Pokemon pokemon={pokemons[this.state.count]} />    
+        </div>      
+
+        <button onClick={ this.handleNextPokemon }>Próximo Pokemon</button>
+      </div>      
+    );
+  }
 }
 
 export default Pokedex;
--- front-end/bloco-12-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/exercise-pokedex-state/src/Pokemon.js
+++ front-end/bloco-12-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/exercise-pokedex-state/src/Pokemon.js
@@ -1,21 +1,20 @@
 import React from 'react';
 
 class Pokemon extends React.Component {
-    render() {
-        // desestruturando as chaves do objeto Pokemons
-        const { name, type, averageWeight, image } = this.props.pokemon;
-
-        return (
-            <div className="pokemon">
-                <div>
-                    <p> {name} </p>
-                    <p> {type} </p>
-                    <p> {`Average weight: ${averageWeight.value} ${averageWeight.measurementUnit}`}</p>
-                </div>
-                <img src={image} alt={`${name} sprite`} />
-            </div>
-        );
-    }
+  render() {
+    // desestruturando as chaves do objeto Pokemons
+    const { name, type, averageWeight, image } = this.props.pokemon;
+    return (
+      <div className="pokemon">
+        <div>
+          <p> {name} </p>
+          <p> {type} </p>
+          <p> {`Average weight: ${averageWeight.value} ${averageWeight.measurementUnit}`}</p>
+        </div>
+        <img src={image} alt={`${name} sprite`} />
+      </div>
+    );
+  }
 }
 
 export default Pokemon;
