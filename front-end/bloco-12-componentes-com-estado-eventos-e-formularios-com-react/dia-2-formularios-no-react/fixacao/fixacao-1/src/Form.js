3b3e3e7d0612f145ccabbcbe281caaf3960e091c
Flavio Pires <flavioasp77@gmail.com>
2021-09-03 15:19:39 -0300

    Exercicio de fixaçao 1 finalizado

--- front-end/bloco-12-componentes-com-estado-eventos-e-formularios-com-react/dia-2-formularios-no-react/fixacao/fixacao-1/src/Form.js
+++ front-end/bloco-12-componentes-com-estado-eventos-e-formularios-com-react/dia-2-formularios-no-react/fixacao/fixacao-1/src/Form.js
@@ -1,9 +1,40 @@
 import React from 'react';
 
 class Form extends React.Component {
+  constructor() {
+    super();
+    this.handleChange = this.handleChange.bind(this);
+    
+    this.state = {
+      name: '',
+      resume: '',
+    }
+
+  }
+
+  handleChange(event) {
+    this.setState({
+      name: event.target.value,
+    })
+  }
+
   render() {
     return (
-
+      <section>
+        <h1>Meu form</h1>
+        <form>
+          <label>Nome:</label>
+          <input type="text" name="name" value={this.state.name} onChange={this.handleChange} />
+          <select name="opcao">
+            <option value="opção 1">opção 1</option>
+            <option value="opção 2">opção 2</option>
+            <option value="opção 3">opção 3</option>
+            <option value="opção 4">opção 4</option>
+          </select>
+          <label>Resumo:</label>
+          <input type="textarea" name="resume" />
+        </form>
+      </section>
     );
   }
 }
