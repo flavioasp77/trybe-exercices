e0e986fd5487961b1174396e51ff91ad47039517
Flavio Pires <flavioasp77@gmail.com>
2021-09-03 16:21:29 -0300

    Exercicio de fizaçao 2 finalizado

--- front-end/bloco-12-componentes-com-estado-eventos-e-formularios-com-react/dia-2-formularios-no-react/fixacao/fixacao-1/src/Form.js
+++ front-end/bloco-12-componentes-com-estado-eventos-e-formularios-com-react/dia-2-formularios-no-react/fixacao/fixacao-1/src/Form.js
@@ -7,32 +7,60 @@ class Form extends React.Component {
     
     this.state = {
       name: '',
+      opcao: '',      
       resume: '',
+      check: false,
     }
 
   }
 
-  handleChange(event) {
-    this.setState({
-      name: event.target.value,
-    })
-  }
+  handleChange({ target }) {
+    const { name } = target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
 
+    this.setState({
+      [name]: value,
+    });
+  }
   render() {
     return (
       <section>
         <h1>Meu form</h1>
         <form>
           <label>Nome:</label>
-          <input type="text" name="name" value={this.state.name} onChange={this.handleChange} />
-          <select name="opcao">
+          <input 
+            type="text" 
+            name="name" 
+            value={this.state.name} 
+            onChange={this.handleChange}
+          />
+          
+          <select 
+            name="opcao" 
+            value={this.state.opcao} 
+            onChange={this.handleChange} 
+          >
             <option value="opção 1">opção 1</option>
             <option value="opção 2">opção 2</option>
             <option value="opção 3">opção 3</option>
             <option value="opção 4">opção 4</option>
           </select>
-          <label>Resumo:</label>
-          <input type="textarea" name="resume" />
+          
+          <label for="resume">Resumo:</label>
+          <textarea 
+            type="textarea"
+            id="resume"
+            name="resume"
+            value={this.state.resume} 
+            onChange={this.handleChange}
+          />
+          <input 
+            type="checkbox" 
+            name="check" 
+            id="check" 
+            onChange={this.handleChange}
+          />
+          <label for="check">Marque ou não marque</label>
         </form>
       </section>
     );
